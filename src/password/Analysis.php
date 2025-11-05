b8703ffa4caebe780b912f8de550107c70dd3221
sticky4loop-hash <sticky4loop@gmail.com>
2025-11-05 08:29:55 +0100

    Clean Analysis class: remove sideeffects and improve UTF-8 handling
    Problem:
    The Analysis class had global UTF-8 settings (mb_internal_encoding and mb_http_output) affecting the entire PHP process, causing side effects.
    
    preg_split('//u', ...) produced empty elements, requiring additional checks and making the code less clean and more error-prone.
    
    Solution:
    Removed global mb_* settings and ensured UTF-8 is handled explicitly in string operations.
    
    Used preg_split with PREG_SPLIT_NO_EMPTY to automatically filter out empty characters.
    
    Used array_count_values to count unique characters efficiently.
    
    Fixed regex calls to operate on the string value of the Password object instead of the object itself.
    
    Benefits:
    No global side effects.
    
    More robust UTF-8 and unique character handling.
    
    Cleaner, easier-to-maintain, and easier-to-test code.
    
    Testing / Notes:
    Existing unit tests should pass unchanged.
    
    Tested manually with Swedish characters (åäö) to ensure proper counting of lowercase, uppercase, numbers, symbols, and unique characters.

--- src/password/Analysis.php
+++ src/password/Analysis.php
@@ -4,89 +4,63 @@ namespace password;
 
 require_once("src/password/Password.php");
 
-
-//To make AnalysisTest::testCountOthers work...
-//NOTE!!! Sideeffects are not nice...
-//TODO: actually not sure on how to handle this...
-mb_internal_encoding('UTF-8');
-mb_http_output('UTF-8');
-
-
-
 /**
-* Uses info from: http://www.passwordmeter.com/
-*/
+ * Uses info from: http://www.passwordmeter.com/
+ */
 class Analysis {
 
-	/**
-	* @var Password $analysed
-	*/
-	private $analysed;
+    /**
+     * @var Password $analysed
+     */
+    private $analysed;
 
-	public function __construct(Password $password) {
-		$this->analysed = $password;
-	}
+    public function __construct(Password $password) {
+        $this->analysed = $password;
+    }
 
-	public function getPassword() {
-		return $this->analysed;
-	}
+    public function getPassword() {
+        return $this->analysed;
+    }
 
-	public function countLowerCase() {
-		$pattern = '/(*UTF8)[a-zåäö]/';
-		preg_match_all($pattern, $this->analysed, $matches);
-		return count($matches[0]);
-	}
+    public function countLowerCase() {
+        $pattern = '/[a-zåäö]/u';
+        preg_match_all($pattern, $this->analysed->getValue(), $matches);
+        return count($matches[0]);
+    }
 
-	public function countUpperCase() {
-		$pattern = '/(*UTF8)[A-ZÅÄÖ]/';
-		preg_match_all($pattern, $this->analysed, $matches);
-		return count($matches[0]);
-	}
+    public function countUpperCase() {
+        $pattern = '/[A-ZÅÄÖ]/u';
+        preg_match_all($pattern, $this->analysed->getValue(), $matches);
+        return count($matches[0]);
+    }
 
-	public function countNumbers() {
-		$pattern = '/(*UTF8)[\d]/';
-		preg_match_all($pattern, $this->analysed, $matches);
-		return count($matches[0]);
-	}
+    public function countNumbers() {
+        $pattern = '/\d/u';
+        preg_match_all($pattern, $this->analysed->getValue(), $matches);
+        return count($matches[0]);
+    }
 
-	public function countSymbols() {
-		return $this->analysed->countLetters() - $this->countNumbers() 
-											   - $this->countUpperCase() 
-											   - $this->countLowerCase();
-	}
+    public function countSymbols() {
+        return $this->analysed->countLetters() - $this->countNumbers() 
+                                               - $this->countUpperCase() 
+                                               - $this->countLowerCase();
+    }
 
-	public function countUnique() {
-		$unique = array();
-		$lowerCaseArray = preg_split('//u', $this->analysed, -1);
-		
-		foreach ($lowerCaseArray as $key => $letter) {
-			if ($letter == "") {
-				//sideeffect from preg_split
-			} else if (isset($unique[$letter]) === false) {
-				$unique[$letter] = 1;
-			} else {
-				$unique[$letter]++;
-			}
-		}
+    public function countUnique() {
+        $letters = array_filter(
+            preg_split('//u', $this->analysed->getValue(), -1, PREG_SPLIT_NO_EMPTY)
+        );
 
-		return count($unique);
-	}
+        $unique = array_count_values($letters);
+        return count($unique);
+    }
 
-	public function countClasses() {
-		$ret = 0;
-		if ($this->countNumbers() > 0) {
-			$ret++;
-		}
-		if ($this->countUpperCase() > 0) {
-			$ret++;
-		}
-		if ($this->countLowerCase() > 0) {
-			$ret++;
-		}
-		if ($this->countSymbols() > 0) {
-			$ret++;
-		}
-		return $ret;
-	}
-
+    public function countClasses() {
+        $ret = 0;
+        if ($this->countNumbers() > 0) $ret++;
+        if ($this->countUpperCase() > 0) $ret++;
+        if ($this->countLowerCase() > 0) $ret++;
+        if ($this->countSymbols() > 0) $ret++;
+        return $ret;
+    }
 }
